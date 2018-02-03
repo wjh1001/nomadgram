85a69abd49e82910d1a1992cee06311a5c7390d3
Andy Woo <cheese.wo@gmail.com>
2018-02-03 19:48:50 +0900

    new database

--- nomadgram/users/models.py
+++ nomadgram/users/models.py
@@ -7,7 +7,9 @@ from django.utils.translation import ugettext_lazy as _
 
 @python_2_unicode_compatible
 class User(AbstractUser):
-    
+
+    """ User Model """
+
     GENDER_CHOICES = (
         ('male', 'Male'),
         ('female', 'Female'),
@@ -20,16 +22,13 @@ class User(AbstractUser):
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
     website = models.URLField(null=True)
     bio = models.TextField(null=True)
-    phone = models.CharField(max_length=140)
+    phone = models.CharField(max_length=140, null=True)
     gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
-    followers = models.ManyToManyField("self",blank=True)
-    following = models.ManyToManyField("self",blank=True)
+    followers = models.ManyToManyField("self", blank=True)
+    following = models.ManyToManyField("self", blank=True)
 
     def __str__(self):
         return self.username
-
-    def get_absolute_url(self):
-        return reverse('users:detail', kwargs={'username': self.username})
 
     @property
     def post_count(self):
