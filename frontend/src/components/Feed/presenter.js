d9eb508542b0f5d8d828d02d5f63da08de239c61
Andy Woo <cheese.wo@gmail.com>
2018-02-04 12:46:24 +0900

    #3-44 Creting the Feed Container

---
+++ frontend/src/components/Feed/presenter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./styles.scss";
+
+
+const Feed = props => "Feed!!";
+
+Feed.propTypes = {
+    loading: PropTypes.bool.isRequired
+};
+
+
+export default Feed;
