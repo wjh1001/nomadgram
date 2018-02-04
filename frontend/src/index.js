524e734672906cc52771d44b2de1a374fddacc53
Andy Woo <cheese.wo@gmail.com>
2018-02-04 11:22:48 +0900

    bug fix block updated, #3-43 Dealing with Blocked Updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,11 +11,11 @@ import { translations } from "translations"
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
     <I18n translations={translations} initialLang="en" fallbackLang="en">
-      <App />
-    </I18n>
+      <ConnectedRouter history={history}>
+        <App />
     </ConnectedRouter>
+    </I18n>
   </Provider>,
   document.getElementById("root")
 );
