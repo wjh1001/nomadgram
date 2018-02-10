419de8a78d9a9e65df4fb4d3bfcd7b78de7c5a82
Andy Woo <cheese.wo@gmail.com>
2018-02-10 16:25:33 +0900

    commentbox styles

--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 import Textarea from "react-textarea-autosize";
 import styles from "./styles.scss";
 
-const CommentBox  = (props, context) => (
-    <form>
-        <Textarea placeholder={context.t("Add a comment...")} />
- 
-   </form>
+const CommentBox = (props, context) => (
+  <form className={styles.commentBox}>
+    <Textarea
+      className={styles.input}
+      placeholder={context.t("Add a comment...")}
+    />
+  </form>
+);
 
-)
 CommentBox.contextTypes = {
-    t: PropTypes.func.isRequired
-}
+  t: PropTypes.func.isRequired
+};
 
 export default CommentBox;
