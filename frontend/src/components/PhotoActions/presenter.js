503015b12175a22b734dc077198a21129b7c107f
Andy Woo <cheese.wo@gmail.com>
2018-02-10 16:21:44 +0900

    style time & fix error

--- frontend/src/components/PhotoActions/presenter.js
+++ frontend/src/components/PhotoActions/presenter.js
@@ -4,38 +4,35 @@ import Ionicon from "react-ionicons";
 import styles from "./styles.scss";
 
 const PhotoActions = (props, context) => (
-    <div>
-        <div>
-            <span className={styles.icon} onClick={props.handleHeartClick}>
-            {props.isLiked ?(
-                <Ionicon icon="ios-heart" fontSize="28px" color="#EB4B59" />
-            ) :(
-                <Ionicon icon="ios-heart-outline" fontSize="28px" color="black" />
-            ) }
-                
-            </span>
-            <span>
-                <Ionicon icon="ios-text-outline" fontSize="28px" color="black" />
-            </span>
-        </div>
-        <span>{props.number === 1 
-            ? context.t("like") 
-            : context.t("likes")}
-        </span>
+  <div className={styles.actions}>
+    <div className={styles.icons}>
+      <span className={styles.icon} onClick={props.handleHeartClick}>
+        {props.isLiked ? (
+          <Ionicon icon="ios-heart" fontSize="28px" color="#EB4B59" />
+        ) : (
+          <Ionicon icon="ios-heart-outline" fontSize="28px" color="black" />
+        )}
+      </span>
+      <span className={styles.icon}>
+        <Ionicon icon="ios-text-outline" fontSize="28px" color="black" />
+      </span>
     </div>
+    <span className={styles.likes}>
+      {props.number}{" "}
+      {props.number === 1 ? context.t("like") : context.t("likes")}
+    </span>
+  </div>
 );
 
-
-PhotoActions.contextTypes = {
-    t: PropTypes.func.isRequired
+PhotoActions.propTypes = {
+  number: PropTypes.number.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  photoId: PropTypes.number.isRequired,
+  handleHeartClick: PropTypes.func.isRequired
 };
 
-
-PhotoActions.propTypes = {
-    number: PropTypes.number.isRequired,
-    isLiked: PropTypes.bool.isRequired,
-    photoid: PropTypes.number.isRequired,
-    handleHeartClick: PropTypes.func.isRequired
-}
+PhotoActions.contextTypes = {
+  t: PropTypes.func.isRequired
+};
 
 export default PhotoActions;
