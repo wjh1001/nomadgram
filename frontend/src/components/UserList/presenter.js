019de72d969bc766d752e52304ae8728c7799179
Andy Woo <cheese.wo@gmail.com>
2018-02-11 09:48:54 +0900

    add closeLikes

--- frontend/src/components/UserList/presenter.js
+++ frontend/src/components/UserList/presenter.js
@@ -1,28 +1,27 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import styles from './styles.scss';
+import styles from "./styles.scss";
 import Loading from "components/Loading";
 import Ionicon from "react-ionicons";
 
-
 const UserList = props => (
-    <div className={styles.container}>
-      <div className={styles.overlay} />
-      <div className={styles.box}>
-        <header className={styles.header}>
-          <h4>{props.title}</h4>
-          <Ionicon icon="md-close" fontSize="20px" color="black" />
-        </header>
-        <div className={styles.content}>{props.loading ? <Loading /> : null}</div>
-      </div>
+  <div className={styles.container}>
+    <div className={styles.box}>
+      <header className={styles.header}>
+        <h4 className={styles.title}>{props.title}</h4>
+        <span onClick={props.closeLikes}>
+        <Ionicon icon="md-close" fontSize="20px" color="black" />
+        </span>
+      </header>
+      <div className={styles.content}>{props.loading ? <Loading /> : null}</div>
     </div>
-  );
-
-
+  </div>
+);
 UserList.propTypes = {
-    title: PropTypes.string.isRequired,
-    loading: PropTypes.bool.isRequired,
-    users: PropTypes.array
+  title: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
+  users: PropTypes.array,
+  closeLikes: PropTypes.func.isRequired
 };
 
 export default UserList;
