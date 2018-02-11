4d7ec63b396c036783c4068788fe08d49851a55c
Andy Woo <cheese.wo@gmail.com>
2018-02-11 09:18:38 +0900

    fix02

--- frontend/src/components/UserList/container.js
+++ frontend/src/components/UserList/container.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import UserList from "./presenter";
 
-
-class Container extends Component{
-    state = {
-        Loading: true
-    };
-    render(){
-        return <UserList {...this.props} {...this.state} />;
-    }
+class Container extends Component {
+  state = {
+    loading: true
+  };
+  render() {
+    return <UserList {...this.props} {...this.state} />;
+  }
 }
-
 
 export default Container;
