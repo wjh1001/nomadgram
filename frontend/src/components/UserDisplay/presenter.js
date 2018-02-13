0b8843bfbb5d25dd3a3509eab4cef8b51dc1e8b2
Andy Woo <cheese.wo@gmail.com>
2018-02-13 09:42:55 +0900

    fix userRow pesenter

--- frontend/src/components/UserDisplay/presenter.js
+++ frontend/src/components/UserDisplay/presenter.js
@@ -4,25 +4,24 @@ import styles from "./styles.scss";
 
 const UserDisplay = (props, context) => (
     <div className={props.horizontal ? styles.horizontal : styles.vertical}>
-        <div className={styles.column}>
-            <img
-                src={props.user.profile_image || require("images/noPhoto.jpg")}
-                alt={props.user.username}
-                className={props.big ? styles.bigAvatar | styles.avatar}
-            />
-            <div className={styles.user}>
-                <span className={styles.username}>{props.user.username}
-                </span>
-                <span className={styles.name}>{props.user.name}
-                </span>
-            </div>
+      <div className={styles.column}>
+        <img
+          src={props.user.profile_image || require("images/noPhoto.jpg")}
+          alt={props.user.username}
+          className={props.big ? styles.bigAvatar : styles.avatar}
+        />
+        <div className={styles.user}>
+          <span className={styles.username}>{props.user.username}</span>
+          <span className={styles.name}>{props.user.name}</span>
         </div>
-        <span className={styles.column} >
-            <button className={styles.button} onClick={props.handelClick}> {props.user.following ? context.t("Unfollow") : context.t("Follow")} </button>
-        </span>
+      </div>
+      <span className={styles.column}>
+        <button className={styles.button} onClick={props.handleClick}>
+          {props.user.following ? context.t("Unfollow") : context.t("Follow")}
+        </button>
+      </span>
     </div>
-
-);
+  );
 
 UserDisplay.propTypes ={
     t.PropTypes.func,
